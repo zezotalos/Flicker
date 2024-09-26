186cf169eced55797d5c7497f171e0428dea13c4
zezotalos <zezotalos@outlook.com>
2024-09-26 00:32:18 +0000

    ......

--- workflow.py
+++ workflow.py
@@ -77,15 +77,15 @@ def create_servers(ids=None , country_codes=None):
             "server_id": "4",
             "country": "France",
         },
-        # {
-        #     "server_id": "108",
-        #     "country": "Germany",
-        # }
-        # },
         {
-            "server_id": "100",
+            "server_id": "108",
             "country": "Germany",
         }
+        # },
+        # {
+        #     "server_id": "100",
+        #     "country": "Germany",
+        # }
     ]
 
     if country_codes:
@@ -205,7 +205,7 @@ if __name__ == "__main__":
 
     if not os.path.exists(configs_path):
         
-        servers = create_servers(country_codes=["FR" , "DE"])
+        servers = create_servers(country_codes=["DE"])
 
         # TEST ALL SERVER BEFORE Putting IT IN THE SUB FILE 
         # Configuration parameters
